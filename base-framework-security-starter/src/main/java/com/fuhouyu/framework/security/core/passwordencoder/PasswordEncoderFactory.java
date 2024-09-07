179b70b06c74143aa09c3e8b98b7d510560524fe
付后钰 <fuhouyu@live.cn>
2024-09-07 23:13:58 +0800

    feat: sm3 密码编码器

---
+++ base-framework-security-starter/src/main/java/com/fuhouyu/framework/security/core/passwordencoder/PasswordEncoderFactory.java
@@ -0,0 +1,55 @@
+package com.fuhouyu.framework.security.core.passwordencoder;
+
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <p>
+ * 密码编码器的工厂类
+ * </p>
+ *
+ * @author fuhouyu
+ * @since 2023/8/30 15:37
+ */
+public class PasswordEncoderFactory {
+
+
+    private PasswordEncoderFactory() {
+
+    }
+
+    /**
+     * 根据encodeId创建一个密码编码器
+     *
+     * @param encodingId 编码id
+     * @return 密码编码器
+     */
+    @SuppressWarnings("deprecation")
+    public static PasswordEncoder createDelegatingPasswordEncoder(String encodingId) {
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+        encoders.put(encodingId, new BCryptPasswordEncoder());
+        encoders.put("ldap", new org.springframework.security.crypto.password.LdapShaPasswordEncoder());
+        encoders.put("MD4", new org.springframework.security.crypto.password.Md4PasswordEncoder());
+        encoders.put("MD5", new org.springframework.security.crypto.password.MessageDigestPasswordEncoder("MD5"));
+        encoders.put("noop", org.springframework.security.crypto.password.NoOpPasswordEncoder.getInstance());
+        encoders.put("pbkdf2", Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_5());
+        encoders.put("pbkdf2@SpringSecurity_v5_8", Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8());
+        encoders.put("scrypt", SCryptPasswordEncoder.defaultsForSpringSecurity_v4_1());
+        encoders.put("scrypt@SpringSecurity_v5_8", SCryptPasswordEncoder.defaultsForSpringSecurity_v5_8());
+        encoders.put("SHA-1", new org.springframework.security.crypto.password.MessageDigestPasswordEncoder("SHA-1"));
+        encoders.put("SHA-256",
+                new org.springframework.security.crypto.password.MessageDigestPasswordEncoder("SHA-256"));
+        encoders.put("sha256", new org.springframework.security.crypto.password.StandardPasswordEncoder());
+        encoders.put("argon2", Argon2PasswordEncoder.defaultsForSpringSecurity_v5_2());
+        encoders.put("argon2@SpringSecurity_v5_8", Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8());
+        encoders.put("sm3", new Sm3PasswordEncoder());
+        return new DelegatingPasswordEncoder(encodingId, encoders);
+    }
+}

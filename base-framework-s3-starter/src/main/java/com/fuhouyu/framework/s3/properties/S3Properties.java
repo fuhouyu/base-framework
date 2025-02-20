3bc3cf1c8c4fc2ebf22f5fbd7426690e67989253
fuhouyu <fuhouyu@live.cn>
2025-02-20 20:22:42 +0800

    feat: minio 修改为s3

--- base-framework-s3-starter/src/main/java/com/fuhouyu/framework/s3/properties/S3Properties.java
+++ base-framework-s3-starter/src/main/java/com/fuhouyu/framework/s3/properties/S3Properties.java
@@ -56,7 +56,29 @@ public class S3Properties {
     private String secretKey;
 
     /**
+     * 是否启用sts
+     */
+    private Boolean stsEnabled;
+
+    /**
+     * 使用路径风格
+     * 如： https://oss.s3.com?bucketName=xxx
+     */
+    private Boolean pathStyleEnabled;
+
+    /**
+     * stsToken
+     */
+    private String stsToken;
+
+
+    /**
      * 区域
      */
     private String region;
+
+    public S3Properties() {
+        this.pathStyleEnabled = false;
+        this.stsEnabled = false;
+    }
 }
